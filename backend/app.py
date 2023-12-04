02342fd483c785eff718461d482af525659a4544
Alessia Volpi <77933342+alessiav8@users.noreply.github.com>
2023-12-04 17:15:46 +0100

    intro scatterplot

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import os
+from mds import create_scatter_plot
 
 app = Flask(__name__, template_folder="../frontend/templates")
 
@@ -7,10 +8,20 @@ app = Flask(__name__, template_folder="../frontend/templates")
 @app.route('/')
 def index():
     template_name = "index.html"
+    
     try:
-        return render_template(template_name)
+        script_directory = os.path.dirname(os.path.abspath(__file__))
+        csv_path = os.path.join(script_directory, 'june2022.xlsx')
+        
+        # Call the function to get the data
+        scatter_data = create_scatter_plot(csv_path)
+        print(scatter_data)
+        # Pass the data to the template
+        return render_template(template_name, scatter_data=scatter_data)
+
     except Exception as e:
-        return f"Error rendering template: {e}"
+        print(f"An error occurred: {e}")
+        return "Error: Failed "
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, threaded=False)
--- backend/mds.py
+++ backend/mds.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import matplotlib
+from flask import Flask
+from sklearn.preprocessing import StandardScaler
+
+
 matplotlib.use('Agg')
+
 def convert_to_int(value):
     if isinstance(value, str):  
         if 'M' in value:
@@ -17,9 +22,8 @@ def convert_to_int(value):
         return value  
 
     
-def create_scatter_plot():
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-    csv_path = os.path.join(script_directory, 'june2022.xlsx')
+def create_scatter_plot(csv_path):
+   
     # Read the Excel file, specifying header=1 because the title is in the first row
     df = pd.read_excel(csv_path, header=1)
     
@@ -32,24 +36,20 @@ def create_scatter_plot():
     features = df[['Followers', 'Avg. views', 'Avg. likes', 'Avg. comments']].values
     features = pd.DataFrame(features)
     features = features.applymap(convert_to_int)
-    print("Data Frame\n",features)
+    scaler = StandardScaler()
+    features_scaled = scaler.fit_transform(features)
 
     # Create a dissimilarity matrix
-    dissM = np.zeros((len(features), len(features)))
+    dissM = np.zeros((len(features_scaled), len(features_scaled)))
 
-    #for now just the euclidean distance between two points, considering their features 
-    for i in range(len(features)):
-        for j in range(len(features)):
-            print("i,\n",features.iloc[i])
-            print("\nj,\n",features.iloc[j])
-
-            dissM[i, j] = np.linalg.norm(features.iloc[i] - features.iloc[j])
+    for i in range(len(features_scaled)):
+        for j in range(len(features_scaled)):
+            dissM[i, j] = np.linalg.norm(features_scaled[i] - features_scaled[j])
 
     # Multidimensional Scaling (MDS)
     mds = MDS(n_components=2, max_iter=300, dissimilarity="precomputed", random_state=42)
     pos = mds.fit_transform(dissM)
 
-    # Scatter plot
     s = 30
     plt.scatter(pos[:, 0], pos[:, 1], color='red', s=s, lw=0, label='d[i]-d[j]')
 
@@ -60,7 +60,12 @@ def create_scatter_plot():
             'x': x,
             'y': y
         })
-        
+    
+    for annotation in annotations:
+        for key, value in annotation.items():
+            if isinstance(value, np.float64):
+                annotation[key] = float(value)
+                #print(annotation,key,value)
 
-    return render_template('index.html', scatter_plot=scatter_plot, annotations=annotations)
+    return annotations
 
